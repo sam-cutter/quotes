83c4e4483188c31928a33af9df367cd37195aa64
Sam Cutter <samuelcutter27@gmail.com>
2023-06-03 17:23:10 +0100

    Added UI functionality to call create session function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { newSession } from "../lib/pocketbase";
+
+    async function handleNewSessionClick() {
+        const session = await newSession();
+
+        goto(`/session/${session.id}`);
+    }
+</script>
+
 <h1>Quotes</h1>
+
+<button on:click={handleNewSessionClick}>Begin new session</button>
