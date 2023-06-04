da9db253e1ac487f8aa8c6c17b836ba8b8b6744d
Sam Cutter <samuelcutter27@gmail.com>
2023-06-04 07:59:47 +0100

    Added PocketBase function to get all quotes

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,13 @@
 import PocketBase from 'pocketbase';
 
+export async function getAllQuotes() {
+	const pb = new PocketBase('http://127.0.0.1:8090');
+
+	const quotes = await pb.collection('quotes').getFullList();
+
+	return quotes;
+}
+
 export async function newSession() {
 	const pb = new PocketBase('http://127.0.0.1:8090');
 
